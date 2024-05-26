371c7375a99da356d84dc30ff945c66cd56e9426
Shuhrat <shoymardonovshuhrat3@gmail.com>
2024-05-26 14:48:36 +0500

    Update task7.dart

--- dart_application_1/bin/task7.dart
+++ dart_application_1/bin/task7.dart
@@ -1,16 +1,1 @@
-import 'dart:io';
-void main(List<String> args) {
-  Function nom = nma();
-  print(nom());
-}
-Function nma(){
-  String arr(String a){
-    if (a.length > 8) {
-      return "true";
-    }
-    else {
-      return "false";
-    }
-  }
-  return arr;
-}
+
