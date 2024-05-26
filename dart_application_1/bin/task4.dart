7e384c2a824617f2a2bd04529df7ad960fb9ad32
Shuhrat <shoymardonovshuhrat3@gmail.com>
2024-05-26 14:42:52 +0500

    Update task4.dart

--- dart_application_1/bin/task4.dart
+++ dart_application_1/bin/task4.dart
@@ -1,12 +1,5 @@
-void main() {
-  List arr = [10, 20, 5, 30, -8, 14, 72, 15];
-  int b = 15;
-  
-  List result = Number(arr, b);
-  print(result); 
+void main(List<String> args) {
+  String s = "hello";
+  var a = s.split('').reversed.join('');
+  print(a);
 }
-List Number(List arr, int b) {
-  List natj = arr.where((element) => element < b).toList();
-  return natj;
-}
-
