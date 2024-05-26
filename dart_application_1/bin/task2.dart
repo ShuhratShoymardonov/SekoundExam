2affd7f83b6c6ae38bd7832d076f19922ca49fb7
Shuhrat <shoymardonovshuhrat3@gmail.com>
2024-05-26 14:42:31 +0500

    Update task2.dart

--- dart_application_1/bin/task2.dart
+++ dart_application_1/bin/task2.dart
@@ -1,6 +1,12 @@
 void main() {
   List arr = [10, 20, 5, 30, -8, 14, 72, 15];
   int b = 15;
-  List natj = arr.where((element) => element < b).toList();
-  print(natj);
+  
+  List result = Number(arr, b);
+  print(result); 
 }
+List Number(List arr, int b) {
+  List natj = arr.where((element) => element < b).toList();
+  return natj;
+}
+
