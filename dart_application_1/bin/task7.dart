ca56b68fe61c46ddb1754c94a0c0f51ef065e6b1
Shuhrat <shoymardonovshuhrat3@gmail.com>
2024-05-27 12:31:49 +0500

    Update task7.dart

--- dart_application_1/bin/task7.dart
+++ dart_application_1/bin/task7.dart
@@ -1,1 +1,16 @@
 
+void main() {
+  Function nom = nma();
+  print(nom("securePass123", 8)); 
+}
+
+Function nma() {
+  String arr(String a ,int b) {
+    if (a.length > b) {
+      return "true";
+    } else {
+      return "false";
+    }
+  }
+  return arr;
+}
